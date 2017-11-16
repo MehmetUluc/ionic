02d8dbe3df4b1cd28c15139cf8c62f3f57707d1c
Mehmet Uluç <mehmet.uluc@zerobuffer.com>
2017-11-16 23:13:28 +0300

    "güncelleme"

--- www/config.js
+++ www/config.js
@@ -1,17 +1,17 @@
 var config = {
     serverUrl: "https://mobile.pekiweb.com/", //url of the serve
     wordPressUrl: "https://mobile.pekiweb.com/", //url of the serve
-    logo: "img/icons_stripe.svg", // login logo path
-    miniLogo: "img/ionic_logo.svg", // header logo path
+    logo: "img/esoes.png", // login logo path
+    miniLogo: "img/esoes.png", // header logo path
     layout: 'grid', // layout of the products
     defaultPaymentMethod: 'paypal', // enable disable cart button on products div
     direction: 'ltr', // direction of the layout right to left or left to right
     languageId: 1, // layout of the products (1 for english,2 for german)
-    currency: "USD", // currency symbol
+    currency: "TRY", // currency symbol
     newProductDuration: 40,  // new product duration in days 
     showCartButton: true, // enable disable cart button on products div
     showFooterTabs: true, // enable disable footer tabs
-    shopName: 'Shop', // this is your shop or company name
+    shopName: 'Mağaza', // this is your shop or company name
     lazyLoadingGif: 'dots', //image lazy loading Gif style(http://ionicframework.com/docs/v1/api/directive/ionSpinner/)
     fbId: '432613600196136', // your facebook app id
     notifTitle: 'Lets Shop',//title text for notificaiton
